44ac1125f673a293de83e254212fc4edd937d345
Issblann <anaisabelpp@outlook.com>
2024-12-28 00:03:15 -0500

    change  env template

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string);
+    const mongoUri = process.env.MONGO_URI || process.env.MONGO_URI_DOCKER;
+    await mongoose.connect(mongoUri as string);
     console.log('MongoDB conectado');
   } catch (error) {
     console.error('MongoDB connection failed', error);
